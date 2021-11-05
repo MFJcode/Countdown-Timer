f57528e9633f307554e934819c0801e2fa4561ad
MFJcode <mfjcode@gmail.com>
2021-11-05 15:43:05 +0100

    countdown-front

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+
+const  daysEl = document.getElementById('days');   
+const  hoursEl = document.getElementById('hours');  
+const  minsEl = document.getElementById('mins');   
+const  secondsEl = document.getElementById('seconds');     
+
+
+
 const newYears = '1 Jan 2022';
 
 function countdown() {
@@ -8,10 +16,14 @@ function countdown() {
     const totalSeconds = (newYarsDate - currentDate) / 1000;
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-    const Seconds = Math.floor(totalSeconds % 60);
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
 
-    console.log(days, hours, minutes, Seconds);
+    daysEl.innerHTML = days; 
+    hoursEl.innerHTML = hours; 
+    minsEl.innerHTML = mins; 
+    secondsEl.innerHTML = seconds; 
+
 
 }
 
